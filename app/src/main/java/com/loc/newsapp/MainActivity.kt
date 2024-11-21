e21d15d7791e9295056ba03af90bfddde25ba5f2
junbin <junbin2438@naver.com>
2024-11-22 04:08:26 +0900

    onBoaring_ViewModel

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.loc.newsapp.domain.AppEntryUseCases
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
+import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.loc.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -21,21 +23,25 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
+    lateinit var useCases: AppEntryUseCases
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
+
         lifecycleScope.launch {
-            appEntryUseCases.readAppEntry().collect {
+            useCases.readAppEntry().collect {
                 Log.d("Test", it.toString())
             }
         }
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    OnBoardingScreen()
+                    val viewModel: OnBoardingViewModel = hiltViewModel()
+                    OnBoardingScreen(
+                        event = viewModel::onEvent
+                    )
                 }
             }
         }
